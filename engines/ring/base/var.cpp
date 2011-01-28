a0620d75c74ed6eacdd73899b30f4e0e2c1bdc40
littleboy <littleboy@users.sf.net>
2011-01-28 01:13:35 -0500

    RING: Implement Var class

--- engines/ring/base/var.cpp
+++ engines/ring/base/var.cpp
@@ -27,10 +27,87 @@
 
 namespace Ring {
 
-Var::Var() {
+//////////////////////////////////////////////////////////////////////////
+// VarEntry
+//////////////////////////////////////////////////////////////////////////
+template<class T>
+void VarEntry<T>::saveLoadWithSerializer(Common::Serializer &s) {
+	// We need to provide a specific specialization for each type
+	// We error out in case there isn't a valid one
+	error("[VarEntry::saveLoadWithSerializer] Invalid specialization");
 }
 
-Var::~Var() {
+template<>
+void VarEntry<byte>::saveLoadWithSerializer(Common::Serializer &s) {
+	s.syncAsUint32LE(_id);
+	s.syncAsByte(_value);
+}
+
+template<>
+void VarEntry<int16>::saveLoadWithSerializer(Common::Serializer &s) {
+	s.syncAsUint32LE(_id);
+	s.syncAsSint16LE(_value);
+}
+
+template<>
+void VarEntry<int32>::saveLoadWithSerializer(Common::Serializer &s) {
+	s.syncAsUint32LE(_id);
+	s.syncAsSint32LE(_value);
+}
+
+template<>
+void VarEntry<double>::saveLoadWithSerializer(Common::Serializer &s) {
+	s.syncAsUint32LE(_id);
+	s.syncAsSint32LE(_value);
+}
+
+template<>
+void VarEntry<Common::String>::saveLoadWithSerializer(Common::Serializer &s) {
+	s.syncAsUint32LE(_id);
+	s.syncString(_value);
+}
+
+//////////////////////////////////////////////////////////////////////////
+/// Var
+//////////////////////////////////////////////////////////////////////////
+void Var::removeAll() {
+	_bytes.clear();
+	_words.clear();
+	_dwords.clear();
+	_strings.clear();
+	_floats.clear();
+}
+
+void Var::saveLoadWithSerializer(Common::Serializer &s) {
+	_bytes.saveLoadWithSerializer(s);
+	_words.saveLoadWithSerializer(s);
+	_dwords.saveLoadWithSerializer(s);
+	_strings.saveLoadWithSerializer(s);
+	_floats.saveLoadWithSerializer(s);
+}
+
+template<typename T>
+void Var::define(AssociativeArray<VarEntry<T>> *array, ID id, T value) {
+	if (array->has(id))
+		error("[Var::define] ID already exists (%d)", id);
+
+	array->push_back(VarEntry<T>(id, value));
+}
+
+template<typename T>
+T Var::get(AssociativeArray<VarEntry<T>> *array, ID id) {
+	if (!array->has(id))
+		error("[Var::define] ID doesn't exists (%d)", id);
+
+	return array->get(id)->get();
+}
+
+template<typename T>
+void Var::set(AssociativeArray<VarEntry<T>> *array, ID id, T value) {
+	if (!array->has(id))
+		error("[Var::define] ID doesn't exists (%d)", id);
+
+	array->get(id)->set(value);
 }
 
 } // End of namespace Ring
--- engines/ring/base/var.h
+++ engines/ring/base/var.h
@@ -26,15 +26,63 @@
 #ifndef RING_VAR_H
 #define RING_VAR_H
 
+#include "ring/shared.h"
+
 namespace Ring {
 
-class Var {
+template<class T>
+class VarEntry : public BaseObject, Common::Serializable {
 public:
-	Var();
-	~Var();
+	VarEntry() : BaseObject(0) {}
+	VarEntry(ID id, T value) : BaseObject(id), _value(value) {}
+
+	void set(T value) { _value = value; }
+	T get() { return _value; }
+
+	// Serializable
+	void saveLoadWithSerializer(Common::Serializer &s);
 
 private:
+	T _value;
+};
 
+#define DEFINE_FUNCTIONS(name, type, storage) \
+	void define##name(ID id, type value) { define(&storage, id, value); } \
+	type get##name(ID id) { return get(&storage, id); } \
+	void set##name(ID id, type value) { set(&storage, id, value); }
+
+class Var : Common::Serializable {
+public:
+	Var() {}
+	~Var() {}
+
+	void removeAll();
+
+	// Accessors
+	DEFINE_FUNCTIONS(Byte,   byte,           _bytes);
+	DEFINE_FUNCTIONS(Word,   int16,          _words);
+	DEFINE_FUNCTIONS(Dword,  int32,          _dwords);
+	DEFINE_FUNCTIONS(String, Common::String, _strings);
+	DEFINE_FUNCTIONS(Float,  double,         _floats);
+
+	// Serializable
+	void saveLoadWithSerializer(Common::Serializer &s);
+
+private:
+	AssociativeArray<VarEntry<byte>>           _bytes;
+	AssociativeArray<VarEntry<int16>>          _words;
+	AssociativeArray<VarEntry<int32>>          _dwords;
+	AssociativeArray<VarEntry<Common::String>> _strings;
+	AssociativeArray<VarEntry<double>>         _floats;
+
+	template<typename T>
+	void define(AssociativeArray<VarEntry<T>> *array, ID id, T value);
+
+	template<typename T>
+	T get(AssociativeArray<VarEntry<T>> *array, ID id);
+
+	template<typename T>
+	void set(AssociativeArray<VarEntry<T>> *array, ID id, T value);
 };
 
 } // End of namespace Ring
