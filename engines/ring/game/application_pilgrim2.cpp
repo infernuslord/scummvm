5ec8d4ff7bc024d6650054c8fcdfd2828b3b9194
Julien <littleboy@users.sourceforge.net>
2011-06-12 22:16:44 -0400

    RING: Implement game initialization for Pilgrim 2

--- engines/ring/game/application_pilgrim2.cpp
+++ engines/ring/game/application_pilgrim2.cpp
@@ -25,9 +25,18 @@
 
 #include "ring/game/application_pilgrim2.h"
 
+#include "ring/base/art.h"
+#include "ring/base/bag.h"
+#include "ring/base/preferences.h"
+#include "ring/base/rotation.h"
+#include "ring/base/saveload.h"
+#include "ring/base/sound.h"
+
 #include "ring/game/event_pilgrim2.h"
 
 #include "ring/graphics/screen.h"
+
+#include "ring/debug.h"
 
 #include "common/textconsole.h"
 
@@ -37,6 +46,8 @@ namespace Ring {
 
 ApplicationPilgrim2::ApplicationPilgrim2(RingEngine *engine) : Application(engine) {
 	_eventHandler = new EventHandlerPilgrim2(this);
+
+	_systemZone = kZone100;
 }
 
 ApplicationPilgrim2::~ApplicationPilgrim2() {
@@ -50,11 +61,78 @@ void ApplicationPilgrim2::initFont() {
 }
 
 void ApplicationPilgrim2::setup() {
-	error("[ApplicationPilgrim2::setup] Not implemented");
+	// Add the list of episodes
+	addEpisode(kZone2, "One",   1);
+	addEpisode(kZone3, "Two",   2);
+	addEpisode(kZone4, "Three", 2);
+	addEpisode(kZone5, "Four",  2);
+	addEpisode(kZone6, "Five",  1);
+
+	// Add the list of zones
+	addZone(kZone1, "A01", "a01", kArchiveArt, kLoadFromCd);
+	addZone(kZone2, "A02", "a02", kArchiveArt, kLoadFromCd);
+	addZone(kZone3, "A03", "a03", kArchiveArt, kLoadFromCd);
+	addZone(kZone4, "A04", "a04", kArchiveArt, kLoadFromCd);
+	addZone(kZone5, "A05", "a05", kArchiveArt, kLoadFromCd);
+
+	// Setup system zone
+	setupZone(kZoneSY, kSetupTypeNone);
+	setSpace(kZone100);
+
+	// Setup cursors
+	ArchiveType archiveType = (_configuration.artCURSOR ? kArchiveArt : kArchiveFile);
+
+	cursorAdd(kCursorDefault,                 "cur_no", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorBusy,                  "cur_busy", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorIdle,                  "cur_idle", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorMove,                   "cur_muv", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorBack,                  "cur_back", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorMenuIdle,          "cur_menuIdle", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorMenuActive,      "cur_menuActive", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorZoom,                  "cur_zoom", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorTake,                  "cur_take", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorAction,              "cur_action", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorActionObject,  "cur_actionObject", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+	cursorAdd(kCursorDragDrop,              "cur_drag", kCursorTypeImage, 1, kLoadFromCursor, archiveType);
+
+	cursorSetOffset(kCursorBusy,         Common::Point(8, 8));
+	cursorSetOffset(kCursorIdle,         Common::Point(17, 1));
+	cursorSetOffset(kCursorMove,         Common::Point(17, 1));
+	cursorSetOffset(kCursorBack,         Common::Point(17, 1));
+	cursorSetOffset(kCursorMenuIdle,     Common::Point(8, 8));
+	cursorSetOffset(kCursorMenuActive,   Common::Point(8, 8));
+	cursorSetOffset(kCursorZoom,         Common::Point(17, 1));
+	cursorSetOffset(kCursorTake,         Common::Point(17, 1));
+	cursorSetOffset(kCursorAction,       Common::Point(17, 1));
+	cursorSetOffset(kCursorActionObject, Common::Point(17, 1));
+	cursorSetOffset(kCursorDragDrop,     Common::Point(8, 8));
+
+	// Setup subtitles
+	subtitleSetColor(Color(255, 255, 255));
+	subtitleSetBackgroundColor(Color(50, 50, 50));
+}
+
+void ApplicationPilgrim2::initData() {
+	_field_74 = true;
+	_field_75 = true;
+	_field_76 = true;
+	_field_77 = false;
+	_field_78 = true;
 }
 
 void ApplicationPilgrim2::initBag() {
-	error("[ApplicationPilgrim2::initBag] Not implemented");
+	_bag->setOrigin(Common::Point(0, 0));
+	_bag->sub_417D40(15, 26, 40, 61);
+	_bag->setBackgroundOffset(Common::Point(0, 0));
+	_bag->sub_417DD0(10);
+	_bag->sub_417D80(0, 0, 30, 448);
+	_bag->sub_417DA0(610, 0, 30, 448);
+	_bag->sub_4192A0(6, 12);
+	_bag->sub_4192C0(622, 12);
+	_bag->sub_417DE0(506, 0);
+	_bag->sub_419280(500);
+	_bag->loadBackground("bagbgr.tga", "", "", "", "", "", "", "bag_h.bmp", _archiveType);
+	_bag->initHotspots();
 }
 
 #pragma endregion
@@ -107,10 +185,6 @@ Visual *ApplicationPilgrim2::createVisual(Id visualId, uint32 a3, uint32 a4, uin
 
 void ApplicationPilgrim2::initZones() {
 	error("[ApplicationPilgrim2::initZones] Not implemented");
-}
-
-void ApplicationPilgrim2::setupZone(ZoneId zone, SetupType type) {
-	error("[ApplicationPilgrim2::setupZone] Not implemented");
 }
 
 #pragma endregion
