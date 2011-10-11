769db345c132e1579fb3178e0b19561bdb22b4f6
Littleboy <littleboy@scummvm.org>
2011-10-11 00:58:14 -0400

    CRYO: Add stub classes for data formats

--- engines/cryo/module.mk
+++ engines/cryo/module.mk
@@ -1,6 +1,18 @@
 MODULE := engines/cryo
 
 MODULE_OBJS := \
+	data/graphics/font.o \
+	data/graphics/hnm.o \
+	data/graphics/jp6.o \
+	data/graphics/sprite.o \
+	data/graphics/spw.o \
+	data/logic/scenario.o \
+	data/logic/wam.o \
+	data/resource/bigfile.o \
+	data/sound/apc.o \
+	data/sound/spp.o \
+	data/sound/synchro.o \
+	data/sound/zik.o \
 	debug.o \
 	detection.o \
 	cryo.o
